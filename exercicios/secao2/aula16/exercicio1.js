b9bab68c537afde6afdde1a12de44a1bcca4fb28
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-10 10:38:16 -0300

    exercise: adicionado exercício da aula 16

---
+++ exercicios/secao2/aula16/exercicio1.js
@@ -0,0 +1,9 @@
+const name = "Luiz Otávio";
+const age = 30;
+const weight = 84;
+const height = 1.8;
+const imc = weight / (height * height);
+
+console.log(
+    `${name} tem ${age} anos, pesa ${weight} kg tem ${height} de altura e seu IMC é de ${imc}`,
+);
