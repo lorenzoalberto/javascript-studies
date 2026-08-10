2a102e8ee8d644a9508735f200f7e48afecbafb4
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-10 14:28:41 -0300

    exercise: adicionar exercício da aula 28

---
+++ exercicios/secao2/aula28/index.js
@@ -0,0 +1,8 @@
+const num = 55.1987555;
+
+console.log(`Raiz quadrada: ${num ** 0.5}`);
+console.log(`${num} é inteiro: ${Number.isInteger(num)}`);
+console.log(`É NaN: ${Number.isNaN(num)}`);
+console.log(`Arredondado para baixo: ${Math.floor(num)}`);
+console.log(`Arredondado para cima: ${Math.ceil(num)}`);
+console.log(`Com duas casas decimais: ${num.toFixed(2)}`);
