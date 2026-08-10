5922d666613134b0292ff956dc49b3903e24f76a
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-10 13:56:06 -0300

    refactor: adapta o exercício da aula 21 para lógica backend apenas

---
+++ exercicios/secao2/aula21/index.js
@@ -0,0 +1,4 @@
+const num1 = 10;
+const num2 = 10;
+
+console.log(`O resultado da soma é ${num1 + num2}`);
--- exercicios/secao2/aula21/script.js
+++
@@ -1,4 +0,0 @@
-const num1 = Number(prompt("Digite o primeiro número"));
-const num2 = Number(prompt("Digite o segundo número"));
-
-alert(`O resultado da soma é ${num1 + num2}`);
