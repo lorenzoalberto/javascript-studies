de6c4f920467351449bd019c8f170f9ce05996be
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-11 10:41:25 -0300

    exercise: adiciona exercício da aula 32

---
+++ exercicios/secao2/aula32/index.js
@@ -0,0 +1,11 @@
+function criaObjeto(nome, sobrenome, idade) {
+    return { nome, sobrenome, idade };
+}
+
+const arrayObjetos = [];
+
+arrayObjetos.push(criaObjeto("João", "Silva", 23));
+arrayObjetos.push(criaObjeto("Maria", "Souza", 30));
+arrayObjetos.push(criaObjeto("Pedro", "Alberto", 19));
+
+console.log(arrayObjetos);
