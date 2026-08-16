d17833b822bb2ff47325fde4934ef25bf587a775
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-16 06:53:59 -0300

    feat: adiciona anotações e exercício da aula 57

---
+++ secao3/aula57/exercicio/index.js
@@ -0,0 +1,10 @@
+const paragrafos = [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "Pellentesque volutpat nulla nec dapibus semper.",
+    "Nunc imperdiet pellentesque faucibus.",
+    "Suspendisse potenti.",
+];
+
+for (const p of paragrafos) {
+    console.log(p);
+}
