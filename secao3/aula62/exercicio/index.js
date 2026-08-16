08f7f6e6b08f4853d09348084e8dd39987b560eb
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-16 15:51:09 -0300

    exercise: adiciona exercicio da aula 62

---
+++ secao3/aula62/exercicio/index.js
@@ -0,0 +1,14 @@
+function fizzBuzz(numero) {
+    const numeroValido =
+        Number.isFinite(numero) && (numero >= 0) & (numero <= 100);
+
+    if (numeroValido) {
+        if (numero % 5 === 0 && numero % 3 === 0) return "FizzBuzz";
+        if (numero % 3 === 0) return "Fizz";
+        if (numero % 5 === 0) return "Buzz";
+    }
+
+    return numero;
+}
+
+console.log(fizzBuzz("a"));
