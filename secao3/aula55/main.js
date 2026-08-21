d2e3da96799c2bee444625521cd7d0fb010129a9
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-21 08:50:52 -0300

    chore: retirando anotações dos comentários

--- secao3/aula55/main.js
+++ secao3/aula55/main.js
@@ -3,13 +3,4 @@ const frutas = ["maçã", "banana", "uva"];
 for (let fruta in frutas) {
     console.log(frutas[fruta]);
 }
-
-/* 
-Em um objeto, ele originalmente mostraria as chaves.
-É possível ter a chave-valor desse modo:
-for (let chave in pessoa) {
-	console.log(chave, pessoa[chave]);
-}
-Isso considerando que pessoas é um objeto e "chave" é uma string, pois a verificação original para obter o valor de um
-objeto é <objeto>['<string da chave>']
-*/
+s;
