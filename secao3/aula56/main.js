b2da6d3a176415de57624d434ca015be6c070122
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-21 09:25:37 -0300

    chore: retirando anotações dos comentários

--- secao3/aula56/main.js
+++ secao3/aula56/main.js
@@ -3,10 +3,3 @@ const frutas = ["maçã", "banana", "uva"];
 for (let fruta of frutas) {
     console.log(fruta);
 }
-
-/* 
-Em JavaScript, o for...in percorre as chaves/propriedades de um objeto. Em um array, essas chaves correspondem aos
-índices (0, 1, 2...). Já o for...of percorre diretamente os valores de estruturas iteráveis, como arrays e strings.
-Para objetos, normalmente usamos for...in; para percorrer os valores de arrays, normalmente usamos for...of.
-Lembrete: objetos não são iteráveis.
-*/
