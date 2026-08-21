833030486d9aff079ed6b4b5696ef6c3ccc46f6b
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-21 16:47:04 -0300

    feat: adiciona exemplo de Factory Functions

---
+++ secao4/aula75/main.js
@@ -0,0 +1,16 @@
+function criarUsuario(nome, idade) {
+    return {
+        nome,
+        idade,
+
+        apresentar() {
+            console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
+        },
+    };
+}
+
+const lorenzo = criarUsuario("Lorenzo", 25);
+const joao = criarUsuario("João", 30);
+
+lorenzo.apresentar();
+joao.apresentar();
