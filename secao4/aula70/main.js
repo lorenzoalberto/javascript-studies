d6d9288463b7502df17a3fe41007a403913551df
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-21 15:01:53 -0300

    feat(clousure): adiciona exemplo de clousure

---
+++ secao4/aula70/main.js
@@ -0,0 +1,16 @@
+function criarDesconto(porcentagem) {
+    function calcular(preco) {
+        return preco - (preco * porcentagem) / 100;
+    }
+
+    return calcular;
+}
+
+const desconto10 = criarDesconto(10);
+const desconto20 = criarDesconto(20);
+
+console.log(desconto10(100)); // 90
+console.log(desconto10(250)); // 225
+
+console.log(desconto20(100)); // 80
+console.log(desconto20(250)); // 200
