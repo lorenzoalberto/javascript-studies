800bda43ae95b8b738888abd2534e64546d5ae85
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-24 16:17:42 -0300

    feat: adiciona anotações complementares ao loop for...in

--- secao3/55_for_in/main.js
+++ secao3/55_for_in/main.js
@@ -1,10 +1,17 @@
 // For in - Estrutura de repetição
-const frutas = ["maçã", "banana", "uva"];
+const pessoa = {
+  nome: "Lorenzo",
+  idade: 25,
+  profissao: "Desenvolvedor",
+};
 
-for (let fruta in frutas) {
-  console.log(frutas[fruta]);
+for (const chave in pessoa) {
+  console.log(chave);
 }
 
 /*
-for..in é muito usado em objetos.
+O objetivo dele é ler índices.
+
+for...in é muito usado em objetos.
+Quando usado em array, conta seus índices.
 */
