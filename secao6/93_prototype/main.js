998b8a40a705de59911dec006ee2359efc47e5b0
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-25 14:49:50 -0300

    feat(prototype): adiciona um exemplo

---
+++ secao6/93_prototype/main.js
@@ -0,0 +1,12 @@
+function Pessoa(nome, idade) {
+  this.nome = nome;
+  this.idade = idade;
+}
+
+Pessoa.prototype.apresentar = function () {
+  return `Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`;
+};
+
+const lorenzo = new Pessoa("Lorenzo", 25);
+
+console.log(lorenzo.apresentar());
