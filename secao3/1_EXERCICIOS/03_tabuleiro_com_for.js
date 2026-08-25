16894f424a0f3001b3537e37a0ed8e18ac853111
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-25 10:46:12 -0300

    chore: adiciona resolução do exercício 3 da seção 3

--- secao3/1_EXERCICIOS/03_tabuleiro_com_for.js
+++ secao3/1_EXERCICIOS/03_tabuleiro_com_for.js
@@ -13,4 +13,17 @@ Saída esperada:
 Desafio extra: altere uma única constante para gerar um tabuleiro 8 x 8.
 */
 
-const tamanho = 4;
+const tamanho = 8;
+
+for (let i = 0; i < tamanho; i++) {
+  let linha = "";
+  for (let j = 0; j < tamanho; j++) {
+    if ((i + j) % 2 === 0) {
+      linha += "#";
+    } else {
+      linha += " ";
+    }
+  }
+
+  console.log(linha);
+}
