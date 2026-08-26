f6652a9da61fa1f97bb9f0bd76ca05898fa7a397
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-26 12:50:00 -0300

    feat: adiciona exemplo de herança

---
+++ secao6/95_heranca/main.js
@@ -0,0 +1,38 @@
+class Animal {
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+  apresentar() {
+    console.log(`Meu nome é ${this.nome} e tenho ${this.idade} anos.`);
+  }
+
+  emitirSom() {
+    console.log("Som genérico de animal.");
+  }
+}
+
+class Cachorro extends Animal {
+  constructor(nome, idade, raca) {
+    // Chama o constructor da classe Animal
+    super(nome, idade);
+
+    this.raca = raca;
+  }
+
+  // Sobrescrevendo um método herdado
+  emitirSom() {
+    console.log("Au au!");
+  }
+
+  mostrarRaca() {
+    console.log(`${this.nome} é da raça ${this.raca}.`);
+  }
+}
+
+const cachorro = new Cachorro("Thor", 4, "Golden Retriever");
+
+cachorro.apresentar(); // herdado de Animal
+cachorro.emitirSom(); // sobrescrito por Cachorro
+cachorro.mostrarRaca(); // próprio de Cachorro
