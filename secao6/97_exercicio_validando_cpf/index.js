d21d04b3e311456f36018fe3516b029dfc9a412f
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-26 14:41:03 -0300

    feat: adiciona exercício 'validador básico de CPF'

---
+++ secao6/97_exercicio_validando_cpf/index.js
@@ -0,0 +1,46 @@
+// Exercício - Validando um CPF (Algoritmo)
+const cpf = "705.484.450-52";
+const cpfLimpo = cpf.replace(/\D+/g, "");
+
+const cpfLimpoArray = [...cpfLimpo];
+const cpfLimpoArrayNumber = cpfLimpoArray.map((digito) => {
+  return (digito = Number(digito)); // não esquecer do return
+});
+
+const cpfArrayAnaliseString = [...cpfLimpo].splice(0, 9);
+const cpfArrayAnalise = cpfArrayAnaliseString.map((digito) => {
+  return (digito = Number(digito)); // não esquecer do return
+});
+
+let acumulador = 0;
+let i = 10;
+for (let numero of cpfArrayAnalise) {
+  acumulador = acumulador + numero * i;
+  i--;
+}
+let primeiroDigito = 11 - (acumulador % 11);
+if (primeiroDigito > 9) {
+  primeiroDigito = 0;
+}
+cpfArrayAnalise.push(primeiroDigito);
+
+acumulador = 0;
+i = 11;
+for (let numero of cpfArrayAnalise) {
+  acumulador = acumulador + numero * i;
+  i--;
+}
+let segundoDigito = 11 - (acumulador % 11);
+if (segundoDigito > 9) {
+  segundoDigito = 0;
+}
+cpfArrayAnalise.push(segundoDigito);
+
+const saoIguais =
+  cpfArrayAnalise.length === cpfLimpoArrayNumber.length &&
+  //verifica se todos satisfazem uma condição
+  cpfArrayAnalise.every((numero, indice) => {
+    return numero === cpfLimpoArrayNumber[indice];
+  });
+
+console.log(saoIguais);
