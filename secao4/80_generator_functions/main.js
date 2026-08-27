c5a9f66ffcd048af0b93b2b665c4bf84415bcbef
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-27 09:01:14 -0300

    refactor: melhora o exemplo de Generator Functions

--- secao4/80_generator_functions/main.js
+++ secao4/80_generator_functions/main.js
@@ -1,21 +1,20 @@
 // Funções geradoras
-function* contador() {
-    console.log("Comecei");
-
-    yield 1;
-
-    console.log("Continuei");
-
-    yield 2;
-
-    console.log("Terminei");
+function* filaAtendimento() {
+  yield "Cliente 1";
+  yield "Cliente 2";
+  yield "Cliente 3";
 }
 
-const numeros = contador();
-console.log(numeros.next());
-console.log(numeros.next());
-console.log(numeros.next());
+const fila = filaAtendimento();
 
-/* 
-É possível usar .done e .value também.
-*/
+console.log(fila.next());
+// { value: "Cliente 1", done: false }
+
+console.log(fila.next());
+// { value: "Cliente 2", done: false }
+
+console.log(fila.next());
+// { value: "Cliente 3", done: false }
+
+console.log(fila.next());
+// { value: undefined, done: true }
