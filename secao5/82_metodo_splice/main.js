676c2eaa9990d72054c6a167831b2800ffc47ac3
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-27 09:11:53 -0300

    refactor: organiza exemplo do método .splice()

--- secao5/82_metodo_splice/main.js
+++ secao5/82_metodo_splice/main.js
@@ -1,19 +1,16 @@
 // Método Splice
 // Sintaxe: <array>.splice(indiceInicial, quantidadeParaRemover, itensParaAdicionar)
 
-// Removendo elementos
 const frutas = ["Maçã", "Banana", "Uva", "Morango"];
 
+// Removendo elementos
 frutas.splice(1, 2);
-
 console.log(frutas);
 
 // Adicionando elementos
 frutas.splice(1, 0, "Morango");
-
 console.log(frutas);
 
 // Substituindo elementos
 frutas.splice(1, 1, "Morango");
-
 console.log(frutas);
