685a843b5bbca0281674aa83d09b906eebcb41ef
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-29 09:35:28 -0300

    feat: melhora o exemplo de reduce em arrays

--- secao5/86_reduce/main.js
+++ secao5/86_reduce/main.js
@@ -1,6 +1,19 @@
 // Reduce - Reduzindo o array
-const numeros = [10, 20, 30, 40];
+const compras = [
+  { preco: 40, quantidade: 2 },
+  { preco: 5, quantidade: 3 },
+];
 
-const total = numeros.reduce((acumulador, numero) => acumulador + numero, 0);
+const totalQuantidadeValor = compras.reduce(
+  (acumulador, compra) => {
+    acumulador = {
+      quantidade: acumulador.quantidade + compra.quantidade,
+      valor: acumulador.valor + compra.preco * compra.quantidade,
+    };
 
-console.log(total);
+    return acumulador;
+  },
+  { quantidade: 0, valor: 0 },
+);
+
+console.log(totalQuantidadeValor);
