acefb992cbb12855fd88abbd42aeecfa0860ca4f
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-29 09:48:18 -0300

    feat: melhora o exemplo de forEach

--- secao5/88_foreach/main.js
+++ secao5/88_foreach/main.js
@@ -1,11 +1,19 @@
+// forEach
 const usuarios = [
-    { id: 1, nome: "Lorenzo", ativo: true },
-    { id: 2, nome: "Carlos", ativo: false },
-    { id: 3, nome: "Ana", ativo: true },
+  { id: 1, nome: "Lorenzo", ativo: true },
+  { id: 2, nome: "Carlos", ativo: false },
+  { id: 3, nome: "Ana", ativo: true },
 ];
 
 usuarios.forEach((usuario) => {
-    if (usuario.ativo) {
-        console.log(`${usuario.nome} está ativo`);
-    }
+  if (usuario.ativo) {
+    console.log(`${usuario.nome} está ativo`);
+  }
 });
+
+/* 
+O forEach fornece o índice automaticamente:
+tarefas.forEach((tarefa, index) => {
+  console.log(`${index + 1}. ${tarefa}`);
+});
+*/
