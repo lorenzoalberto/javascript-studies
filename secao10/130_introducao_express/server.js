87c0e518f775d5221302dded6eb7e2e5446eb958
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-08-29 12:03:10 -0300

    feat: adiciona exemplo de código para introdução ao Express

---
+++ secao10/130_introducao_express/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/contato", (req, res) => {
+  res.send("Obrigado por entrar em contato.");
+});
+
+app.listen(3000, () => {
+  console.log("Acessar http://localhost:3000");
+});
